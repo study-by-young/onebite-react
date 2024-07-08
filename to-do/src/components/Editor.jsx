1724b41eb68b21d0abcbbb1b46a2bba56fdeccfa
xxyoungkim <xxyoungkim@gmail.com>
2024-07-08 10:20:06 +0900

    투두 추가하기(Create)

--- to-do/src/components/Editor.jsx
+++ to-do/src/components/Editor.jsx
@@ -1,10 +1,42 @@
 import "./Editor.css";
+import { useState, useRef } from "react";
 
-const Editor = () => {
+const Editor = ({ onCreate }) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onKeyDown = (e) => {
+    //사용자가 키보드를 누를 때 발생하는 이벤트 처리
+    //keyCode 13: Enter
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  };
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  };
+
+  const onSubmit = () => {
+    //내용을 작성하지 않으면 onCreate가 실행되지 않도록 조건 처리
+    if (content === "") {
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent(""); //투두 추가 후 input 비우기
+  };
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 투-두..." />
-      <button>추가</button>
+      <input
+        ref={contentRef}
+        value={content}
+        onKeyDown={onKeyDown}
+        onChange={onChangeContent}
+        placeholder="새로운 투두..."
+      />
+      <button onClick={onSubmit}>추가</button>
     </div>
   );
 };
