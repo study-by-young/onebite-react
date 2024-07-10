3040abba3189a3a2a0b06f9c0100bd8702baef81
xxyoungkim <xxyoungkim@gmail.com>
2024-07-10 17:16:48 +0900

    memo와 컴포넌트 렌더링 최적화

--- to-do/src/components/Header.jsx
+++ to-do/src/components/Header.jsx
@@ -1,4 +1,5 @@
 import "./Header.css";
+import { memo } from "react";
 
 const Header = () => {
   return (
@@ -9,4 +10,8 @@ const Header = () => {
   );
 };
 
-export default Header;
+//인수: 최적화하고 싶은 컴포넌트
+//memo: props가 변경되지 않았을 때는 리렌더링 하지 않도록 최적화해서 반환
+const memoizedHeader = memo(Header);
+
+export default memoizedHeader;
--- to-do/src/components/ToDoItem.jsx
+++ to-do/src/components/ToDoItem.jsx
@@ -1,4 +1,5 @@
 import "./ToDoItem.css";
+import { memo } from "react";
 
 const ToDoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   const onChangeCheckbox = () => {
@@ -24,4 +25,16 @@ const ToDoItem = ({ id, isDone, content, date, onUpdate, onDelete }) => {
   );
 };
 
-export default ToDoItem;
+//고차 컴포넌트(HOC, Higher Order Component)
+//컴포넌트를 인수로 받아서 해당 컴포넌트에 추가 기능을 덧붙여서 기능이 추가된 새로운 컴포넌트를 반환하는 메서드
+//한 번 호출하는 것만으로도 컴포넌트에 새로운 기능을 부여할 수 있음
+export default memo(ToDoItem, (prevProps, nextProps) => {
+  //변환값에 따라 props 변경 여부 판단
+  //True -> props 바뀌지 않음 -> 리렌더링 X
+  //False -> props 바뀜 -> 리렌더링 O
+  if (prevProps.id !== nextProps.id) return false;
+  if (prevProps.isDone !== nextProps.isDone) return false;
+  if (prevProps.content !== nextProps.content) return false;
+  if (prevProps.date !== nextProps.date) return false;
+  return true;
+});
