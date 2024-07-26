3f6dc8f880d2786699307195fd7fa5ec5f90e86a
xxyoungkim <xxyoungkim@gmail.com>
2024-07-26 17:14:20 +0900

    라우팅 설정

--- emotion-diary/src/App.jsx
+++ emotion-diary/src/App.jsx
@@ -1,10 +1,35 @@
 import "./App.css";
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Diary from "./pages/Diary";
+import New from "./pages/New";
+import Notfound from "./pages/Notfound";
 
+/* 
+  1. "/" : 모든 일기를 조회하는 Home 페이지
+  2. "/new" : 새로운 일기를 작성하는 New 페이지
+  3. "/diary" : 일기 상세조회 Diary 페이지
+*/
 function App() {
   return (
-    <>
-      <div>emotion-diary</div>
-    </>
+    <Routes>
+      <Route
+        path="/"
+        element={<Home />}
+      />
+      <Route
+        path="/new"
+        element={<New />}
+      />
+      <Route
+        path="/diary"
+        element={<Diary />}
+      />
+      <Route
+        path="*"
+        element={<Notfound />}
+      />
+    </Routes>
   );
 }
 
