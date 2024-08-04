f5f77b0b6e6b72a77f0f6590276eb6975f9dda76
xxyoungkim <xxyoungkim@gmail.com>
2024-08-04 14:49:43 +0900

    Reducer 생성, 임시데이터 생성

--- emotion-diary/src/App.jsx
+++ emotion-diary/src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useReducer } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Diary from "./pages/Diary";
@@ -6,16 +7,28 @@ import New from "./pages/New";
 import Edit from "./pages/Edit";
 import Notfound from "./pages/Notfound";
 
-import Button from "./components/Button";
-import Header from "./components/Header";
+const mockData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: "1번 일기 내용",
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: "2번 일기 내용",
+  },
+];
 
-/* 
-  1. "/" : 모든 일기를 조회하는 Home 페이지
-  2. "/new" : 새로운 일기를 작성하는 New 페이지
-  3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
-  4. "/edit" : 일기를 수정하는 Edit 페이지
-*/
+function reducer(state, action) {
+  return state;
+}
+
 function App() {
+  const [data, dispatch] = useReducer(reducer, mockData);
+
   return (
     <>
       <Routes>
