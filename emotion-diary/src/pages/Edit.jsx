f8d8201f800dedf06be16311820fd0ed33873733
xxyoungkim <xxyoungkim@gmail.com>
2024-08-12 22:30:18 +0900

    Diary 페이지 UI, 기능 구현
    - Custom Hook 만들기
    - 자주 쓰는 함수 모듈 분리하기

--- emotion-diary/src/pages/Edit.jsx
+++ emotion-diary/src/pages/Edit.jsx
@@ -1,29 +1,17 @@
-import { useParams, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryDispatchContext } from "../App";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
-import { useContext, useEffect, useState } from "react";
-import { DiaryDispatchContext, DiaryStateContext } from "../App";
+import useDiary from "../hooks/useDiary";
 
 const Edit = () => {
   const params = useParams();
   const nav = useNavigate();
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
-  const data = useContext(DiaryStateContext);
-  const [curDiaryItem, setCurDiaryItem] = useState();
 
-  useEffect(() => {
-    const currentDiaryItem = data.find(
-      (item) => String(item.id) === String(params.id)
-    );
-
-    if (!currentDiaryItem) {
-      window.alert("존재하지 않는 일기입니다.");
-      nav("/", { replace: true });
-    }
-
-    setCurDiaryItem(currentDiaryItem);
-  }, [params.id, data]);
+  const curDiaryItem = useDiary(params.id);
 
   const onClickDelete = () => {
     const isConfirm = window.confirm(
@@ -54,24 +42,12 @@ const Edit = () => {
     <div>
       <Header
         title={"일기 수정하기"}
-        leftChild={
-          <Button
-            text={"< 뒤로 가기"}
-            onClick={() => nav(-1)}
-          />
-        }
+        leftChild={<Button text={"< 뒤로 가기"} onClick={() => nav(-1)} />}
         rightChild={
-          <Button
-            text={"삭제하기"}
-            type={"NEGATIVE"}
-            onClick={onClickDelete}
-          />
+          <Button text={"삭제하기"} type={"NEGATIVE"} onClick={onClickDelete} />
         }
       />
-      <Editor
-        initData={curDiaryItem}
-        onSubmit={onSubmit}
-      />
+      <Editor initData={curDiaryItem} onSubmit={onSubmit} />
     </div>
   );
 };
