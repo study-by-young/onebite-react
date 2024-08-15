40e9a05b39507da550c76a20d66eae1ba2b28e24
xxyoungkim <xxyoungkim@gmail.com>
2024-08-15 14:42:21 +0900

    배포 사전 준비 작업
    - 페이지 타이틀 설정
    - Favicon 설정
    - 오픈 그래프 태그 설정
    - 프로젝트 빌드

--- emotion-diary/src/pages/Edit.jsx
+++ emotion-diary/src/pages/Edit.jsx
@@ -5,6 +5,7 @@ import Button from "../components/Button";
 import Editor from "../components/Editor";
 import Header from "../components/Header";
 import useDiary from "../hooks/useDiary";
+import usePageTitle from "../hooks/usePageTitle";
 
 const Edit = () => {
   const params = useParams();
@@ -12,6 +13,8 @@ const Edit = () => {
   const { onDelete, onUpdate } = useContext(DiaryDispatchContext);
 
   const curDiaryItem = useDiary(params.id);
+
+  usePageTitle(`${params.id}번 일기 수정`);
 
   const onClickDelete = () => {
     const isConfirm = window.confirm(
--- emotion-diary/src/pages/New.jsx
+++ emotion-diary/src/pages/New.jsx
@@ -1,13 +1,16 @@
-import Header from "../components/Header";
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { DiaryDispatchContext } from "../App";
 import Button from "../components/Button";
 import Editor from "../components/Editor";
-import { useNavigate } from "react-router-dom";
-import { useContext } from "react";
-import { DiaryDispatchContext } from "../App";
+import Header from "../components/Header";
+import usePageTitle from "../hooks/usePageTitle";
 
 const New = () => {
   const { onCreate } = useContext(DiaryDispatchContext);
   const nav = useNavigate();
+
+  usePageTitle("새 일기 쓰기");
 
   const onSubmit = (input) => {
     onCreate(input.createdDate.getTime(), input.emotionId, input.content);
@@ -18,12 +21,7 @@ const New = () => {
     <div>
       <Header
         title={"새 일기 쓰기"}
-        leftChild={
-          <Button
-            text={"< 뒤로 가기"}
-            onClick={() => nav(-1)}
-          />
-        }
+        leftChild={<Button text={"< 뒤로 가기"} onClick={() => nav(-1)} />}
       />
       <Editor onSubmit={onSubmit} />
     </div>
